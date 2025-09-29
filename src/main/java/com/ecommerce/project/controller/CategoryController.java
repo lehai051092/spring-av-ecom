d1546599163202f576a294bbff8c9a1a61bf4f0b
HaiLT <le.thanh.hai@sun-asterisk.com>
2025-09-29 21:55:50 +0700

    setting up category service

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,6 +1,5 @@
 package com.ecommerce.project.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,20 +7,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.service.CategoryService;
 
 @RestController
 public class CategoryController {
 
-    private List<Category> categories = new ArrayList<>();
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
 
     @GetMapping("/api/public/categories")
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryService.getAllCategories();
     }
 
     @PostMapping("/api/admin/categories")
     public String createCategory(Category category) {
-        categories.add(category);
+        categoryService.createCategory(category);
         return "Category added successfully";
     }
 }
