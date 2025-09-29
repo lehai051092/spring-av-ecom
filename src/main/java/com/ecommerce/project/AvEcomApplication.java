e983c1161b1b130b18441bcb3eb55dcd756dd056
HaiLT <le.thanh.hai@sun-asterisk.com>
2025-09-29 21:15:19 +0700

    first commit

---
+++ src/main/java/com/ecommerce/project/AvEcomApplication.java
@@ -0,0 +1,12 @@
+package com.ecommerce.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvEcomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvEcomApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ecommerce/project/AvEcomApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ecommerce.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvEcomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
