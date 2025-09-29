40d864ab970cd7c0185aa8535585010daa7c83ed
HaiLT <le.thanh.hai@sun-asterisk.com>
2025-09-29 22:08:16 +0700

    challenge managing ID

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.project.model.Category;
@@ -24,7 +25,7 @@ public class CategoryController {
     }
 
     @PostMapping("/api/admin/categories")
-    public String createCategory(Category category) {
+    public String createCategory(@RequestBody Category category) {
         categoryService.createCategory(category);
         return "Category added successfully";
     }
