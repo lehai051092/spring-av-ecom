ca891d8adc5a90dc8363443763a861be29a773ca
HaiLT <le.thanh.hai@sun-asterisk.com>
2025-10-08 21:55:21 +0700

    setting up product module

---
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -0,0 +1,34 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long productId;
+    private String productName;
+    private String description;
+    private Integer quantity;
+    private String image;
+    private double price;
+    private double discount;
+    private double specialPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
